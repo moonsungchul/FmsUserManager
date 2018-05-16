18ab7651c2245d46013fb179415b625e4468448a
moonstar <moonstar@localhost>
2018-05-16 17:26:52 +0900

    init

---
+++ FmsUserManager/src/main/java/com/firemstar/fum/FmsUserManagerApplication.java
@@ -0,0 +1,12 @@
+package com.firemstar.fum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsUserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsUserManagerApplication.class, args);
+	}
+}
---
+++ FmsUserManager/src/test/java/com/firemstar/fum/FmsUserManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.firemstar.fum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FmsUserManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
